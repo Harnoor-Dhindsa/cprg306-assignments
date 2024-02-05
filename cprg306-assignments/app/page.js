29abf51e304a348608bd96619a1d23b4681bbc62
harnoordhindsa <Harnoor.Dhindsa@edu.sait.ca>
2024-02-04 19:03:18 -0700

    Week-3 Assignment Done

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col justify-between p-24">
       <div>
         <h1 className="text-4xl font-bold py-2 px-20">CPRG 306: Web Development 2 - Assignments</h1>
-        <div className="hover:text-green-500 text-lg px-20 py-5 hover:underline">
+        <div className="text-lg px-20 py-5">
         <Navigation/>
         </div>
       </div>
--- cprg306-assignments/components/Navigation.jsx
+++ cprg306-assignments/components/Navigation.jsx
@@ -2,8 +2,9 @@ import Link from "next/link"
 
 const Navigation = () => {
     return(
-        <div>
-            <Link href="./week-2">Week 2 Assignment</Link>
+        <div className="grid grid-rows-2">
+            <Link href="./week-2" className="hover:text-green-500  hover:underline">Week 2 Assignment</Link>
+            <Link href="./week-3" className="hover:text-green-500  hover:underline">Week 3 Assignment</Link>
         </div>
     )
 }
