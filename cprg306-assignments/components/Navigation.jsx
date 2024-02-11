311e67e9dcd84d5cfd02757ac8ed528ad2249d76
harnoordhindsa <Harnoor.Dhindsa@edu.sait.ca>
2024-02-10 20:12:03 -0700

    Week-4 Done

--- cprg306-assignments/components/Navigation.jsx
+++ cprg306-assignments/components/Navigation.jsx
@@ -5,6 +5,7 @@ const Navigation = () => {
         <div className="grid grid-rows-2">
             <Link href="./week-2" className="hover:text-green-500  hover:underline">Week 2 Assignment</Link>
             <Link href="./week-3" className="hover:text-green-500  hover:underline">Week 3 Assignment</Link>
+            <Link href="./week-4" className="hover:text-green-500  hover:underline">Week 4 Assignment</Link>
         </div>
     )
 }
